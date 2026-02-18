6f5172953f29fb845b409dfc39de5c91ce39b105
thomaspeck <thomaspeck@users.noreply.github.com>
2026-02-18 12:57:15 -0600

    Update simple-plugin.js

--- simple-plugin.js
+++ simple-plugin.js
@@ -1,3 +1,4 @@
+/**
 Draw.loadPlugin(function(ui) {
     // Test: Add a simple menu item
     ui.actions.addAction('testPlugin', function() {
@@ -8,4 +9,16 @@ Draw.loadPlugin(function(ui) {
     menu.addItem(ui, 'Test Plugin', null, 'testPlugin');
     
     console.log('Minimal test plugin loaded!');
+});
+**/
+
+Draw.loadPlugin(function(ui) {
+  // Your custom plugin code goes here
+  alert('My custom plugin is running!');
+
+  // Example of adding a custom menu item
+  var menu = ui.menus.get('plugins');
+  menu.addItem('My Custom Action', null, function() {
+    alert('Custom action triggered!');
+  }, menu.div);
 });
